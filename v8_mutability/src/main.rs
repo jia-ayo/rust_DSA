c7c358dd5bb9cb7556b06b4626f3a51c28ae5f7e
jia-ayo <milkjia11@gmail.com>
2023-01-14 10:19:33 +0100

    mutability, clone

--- v8_mutability/src/main.rs
+++ v8_mutability/src/main.rs
@@ -3,6 +3,16 @@ pub struct People{
     name: String,
     age: i32,
 }
+#[derive(Debug, Clone, Copy)]
+pub struct Point{
+    x: i32,
+    y: i32,
+}
+impl Point{
+    pub fn new(x:i32, y:i32)->Self{
+        Point{x,y}
+    }
+}
 fn main() {
     let mut  x = 34;
     let y = x;
@@ -14,8 +24,12 @@ fn main() {
         age: 19,
     };
     let p2 = p.clone();
-
     p.name.push_str("-ayo");
+    println!("p = {:?}\np2={:?}", p, p2);
 
-    println!("p = {:?}\np2={:?}", p, p2)
+    let mut pnt = Point::new(3,4);
+    let pnt2 =pnt;
+    pnt.x += 3;
+    println!("pnt = {:?}\npnt2={:?}", pnt, pnt2);
+
 }
