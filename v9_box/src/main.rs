c2714b93f1b20b30d57a2e2b8e244b6a5f62531c
jia-ayo <milkjia11@gmail.com>
2023-01-16 03:33:12 +0100

    str and string

--- v9_box/src/main.rs
+++ v9_box/src/main.rs
@@ -27,7 +27,35 @@ fn main() {
     for i in 0..105{
         v.push(i.to_string())
     }
+
     println!("vectores lpength {}, capacity of vec {}", v.len(), v.capacity());
 
     println!("Hello, {:?}", ll);
+    let s = " hello ".to_string();
+    let p = s.trim();
+    let p = p.to_string();
+    println!("p === '{}'",p );
+
+    let fstr= "help me find home";
+    let ffstra = string_find_f(fstr);
+    println!("ffstra= '{}'", ffstra);
+    println!("choose = '{}'", choose_str(1));
+}
+
+fn string_find_f<'a>(s: &'a str)-> &'a str{
+    let n= 0;
+    for (n, x) in s.char_indices(){
+        if x == 'f'{
+            return &s[n..];
+        }
+    }
+    s
+}
+
+fn choose_str(n: i32) -> &'static str{
+    match n {
+        0 => "hello",
+        1 => "goodbye",
+        _ => "others",
+    }
 }
