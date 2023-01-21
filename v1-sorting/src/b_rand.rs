174c242fb95ebb93a07bab09a924b68a31db5cb5
jia-ayo <milkjia11@gmail.com>
2023-01-21 19:31:13 +0100

    before rust analyzer broke

--- v1-sorting/src/b_rand.rs
+++ v1-sorting/src/b_rand.rs
@@ -1,12 +1,12 @@
 use std::sync::Mutex;
 
-lazy_static::lazy_static!{
-   static ref RG:Mutex<RandGen> = Mutex::new(RandGen::new(34052));
-}
+// lazy_static::lazy_static!{
+//    static ref RG:Mutex<RandGen> = Mutex::new(RandGen::new(3504));
+// }
 
-pub fn rand(max:usize)->usize{
-    RG.lock().unwrap().next_v(max)
-}
+// pub fn rand(max:usize)->usize{
+//     RG.lock().unwrap().next_v(max)
+// }
 pub struct RandGen{
     curr: usize,
     mul: usize,
