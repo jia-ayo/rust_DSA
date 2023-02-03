0a6aed2ceb584d214ee8ddf8ef81a528a7896c91
jia-ayo <milkjia11@gmail.com>
2023-02-03 16:56:44 +0100

    map point base

--- v4/graph/src/main.rs
+++ v4/graph/src/main.rs
@@ -3,6 +3,18 @@ use std::collections::HashMap;
 use std::hash::Hash;
 use std::rc::{Weak, Rc};
 
+#[derive(Debug)]
+pub struct GraphErr{
+    mass:String,
+}
+
+impl GraphErr{
+    pub fn new(s: &str)-> Self{
+        GraphErr{
+            mass: s.to_string(),
+        }
+    }
+}
 type Rcc<T> = Rc<RefCell<T>>;
 pub fn rcc<T>(t: T) -> Rcc<T>{
     Rc::new(RefCell::new(t))
@@ -31,16 +43,56 @@ pub struct RccNode<T, E>{
 //Map based
 //Maps point from  key to value normally quickly eg Hashmap
 pub struct MapGraph<T, E, ID:Hash + Eq>{
-    mp: HashMap<ID, T>,
+    data: HashMap<ID, T>,
     edges:  Vec<(E, ID, ID)>,
 }
 
 //mappoint based
+#[derive(Debug)]
 pub struct MapPGraph<T, E, ID:Hash + Eq>{
-    mp: HashMap<ID, (T, Vec<ID>)>,
+    data: HashMap<ID, (T, Vec<ID>)>,
     edges:  HashMap<ID, (E, ID, ID)>,
 }
 
-fn main() {
-    println!("Hello, world!");
+impl <T, E, ID: Clone + Hash + Eq> MapPGraph<T, E, ID>{
+    pub fn new()->Self{
+        MapPGraph { 
+            data: HashMap::new(), 
+            edges: HashMap::new() 
+        }
+    }
+
+    pub fn add_node(&mut self, id:ID, dt:T){
+        //node as no edges yet
+        self.data.insert(id, (dt,Vec::new()));
+    }
+
+    pub fn add_edge(&mut self, ed_id:ID, from:ID, to:ID, edat: E)->Result<(),GraphErr>{
+        if ! self.data.contains_key(&from){
+            return  Err(GraphErr::new("'from' is not in node"));
+        }
+        if let Some(ref mut dt) = self.data.get_mut(&to){
+            self.edges.insert(ed_id.clone(),(edat, from.clone(),to));
+            dt.1.push(ed_id.clone());
+        }else{
+            return Err(GraphErr::new("'to' not in nodes"));
+        }
+        self.data.get_mut(&from).unwrap().1.push(ed_id);
+        Ok(())
+    }
+}
+fn main() ->Result<(), GraphErr> {
+    let mut g = MapPGraph::new();
+    for x in vec!['A', 'B','C','D','E', 'F','G','H']{
+        g.add_node(x, ());
+    }
+    g.add_edge('a', 'H', 'D', 6)?;
+    g.add_edge('b', 'D', 'C', 18)?;
+    g.add_edge('c', 'C', 'B', 10)?;
+    g.add_edge('d', 'H', 'A', 7)?;
+    g.add_edge('e', 'A', 'C', 4)?;
+    g.add_edge('f', 'H', 'G', 5)?;
+    g.add_edge('g', 'G', 'A', 8)?;
+    println!("Hello, GRAPH {:?}",g);
+    Ok(())
 }
