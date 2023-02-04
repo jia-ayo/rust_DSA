91e17cc04e3d05aaa1ee0fdb1bbf125f7575b80d
jia-ayo <milkjia11@gmail.com>
2023-02-04 12:55:03 +0100

    staredted route

--- v4/graph/src/main.rs
+++ v4/graph/src/main.rs
@@ -1,11 +1,56 @@
+use core::fmt;
 use std::cell::RefCell;
 use std::collections::HashMap;
+use std::fmt::write;
 use std::hash::Hash;
 use std::rc::{Weak, Rc};
 
 #[derive(Debug)]
 pub struct GraphErr{
     mass:String,
+}
+
+pub trait Weighted{
+      fn weight(&self)->i32;
+}
+impl Weighted for i32{
+    fn weight(&self)->i32 {
+        *self
+    }
+}
+
+#[derive(Debug)]
+pub struct  Route<ID>{
+    pos: ID,
+    path: Option<Rc<Route<ID>>>,
+    len:i32
+}
+
+impl<ID:Eq> Route<ID>{
+    pub fn start_rc(pos:ID)->Rc<Self>{
+        Rc::new(Route { 
+            pos,
+            path: None, 
+            len: 0,
+        })
+    }
+    pub fn contains (&self, id:&ID)->bool{
+        if self.pos == *id{
+            return true;
+        }
+        match self.path {
+          Some(ref p)=> p.contains(id),
+          None=>false
+        }
+    }
+}
+
+impl <ID:fmt::Debug> fmt::Display for Route<ID>{
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        if let Some(ref p)= self.path;
+        write!(f, "{}-{}-",p,self.ln)
+    }
+    write!(f, ":?", self.pos); 
 }
 
 impl GraphErr{
