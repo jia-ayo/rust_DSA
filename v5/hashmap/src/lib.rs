ad9eacda15157b99caf2f04ff1cb0f364f2ab14f
jia-ayo <milkjia11@gmail.com>
2023-02-10 08:38:19 +0100

    hashmap building

--- v5/hashmap/src/lib.rs
+++ v5/hashmap/src/lib.rs
@@ -1,7 +1,10 @@
 mod hasher;
-use std::hash::Hash;
+use std::{hash::Hash, fmt::Debug};
 use hasher::hash;
 use std::borrow::Borrow;
+
+const BSIZE:usize=8;
+const BGROW:usize = 8;
 
 #[derive(Debug)]
 pub struct  BucketList<K,V>{
@@ -18,7 +21,7 @@ impl <K:  Hash+Eq, V> BucketList<K,V>{
            buckets: vec![Vec::new()],
        }
     }
-    //usixe returned how big chosen bucket is
+    //usize returned how big chosen bucket is
     //tell caller if its too full
     fn push(&mut self, k:K,v:V)->usize{
         let h = (hash(self.seed,&k) as usize )% self.buckets.len();
@@ -68,6 +71,79 @@ impl <K:  Hash+Eq, V> BucketList<K,V>{
             self.buckets.push(Vec::new())
         }
     }
+}
+
+#[derive(Debug)]
+pub struct HMap<K,V>{
+    n_moved: usize,
+    main: BucketList<K,V>,
+    grow: BucketList<K,V>
+}
+
+impl <K:Hash + Eq,V> HMap<K,V>{
+    pub fn new()->Self{
+        HMap { 
+            n_moved: 0, 
+            main: BucketList::new(), 
+            grow:BucketList::new() 
+        }
+    }
+    pub fn insert(&mut self,k:K, v:V){
+        if let Some(iv)=self.main.get_mut(&k) {
+            *iv = v;
+            return;
+        }
+        if let Some(iv) = self.grow.get_mut(&k){
+           *iv = v;
+           return; 
+        }
+        if self.n_moved >0{
+            // we have started move to a bigger bucket list
+            self.grow.push(k,v);
+            self.move_bucket();
+            //TODO grow buckets more 
+            return;
+        }
+        if self.main.push(k,v)>BSIZE/2{
+           //grow buckets
+           self.move_bucket();
+        }
+    }
+    pub fn get<KR>(&self,kr:&KR)->Option<&V>
+        where K:Borrow<KR>,
+             KR:Hash+Eq+?Sized,
+    {
+        self.main.get(kr).or_else(|| self.grow.get(kr))
+      
+    }
+    pub fn get_mut<KR>(&mut self,kr:&KR)->Option<&mut V>
+        where K:Borrow<KR>,
+             KR:Hash+Eq+?Sized
+    {
+        if let Some(b) = self.main.get_mut(kr){
+            return  Some(b);
+        }
+        self.grow.get_mut(kr)
+    }
+    pub fn len(&self)->usize{
+        self.main.len + self.grow.len
+    }
+    pub fn move_bucket(&mut self){
+        if self.n_moved == 0{
+            self.grow.set_buckets(self.main.buckets.len() + BGROW);
+        }
+        if let Some(b)= self.main.bucket(self.n_moved){
+            for (k,v) in b{
+                self.grow.push(k,v);
+            }
+            self.n_moved +=1;
+            return;
+        }
+        //if all data our of main and 
+        std::mem::swap(&mut self.main,&mut self.grow);
+        self.n_moved = 0;
+    }
+    
 }
 // #[cfg(test)]
 // mod tests {
