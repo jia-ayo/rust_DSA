c359ec0db46785ebd3442b106b0c2c7495cb6af9
jia-ayo <milkjia11@gmail.com>
2023-02-19 03:40:50 +0100

    cobined in main

--- v6/ecs/src/gen.rs
+++ v6/ecs/src/gen.rs
@@ -1,78 +1,49 @@
-#[derive(Copy,Clone, PartialEq, Debug)]
-pub struct GenData{
+#[derive(Copy, Clone,PartialEq)]
+pub struct GenData {
     pub pos: usize,
     pub gen: u64,
 }
 
 pub struct EntityActive{
-    active: bool,
-    gen: u64,
-}
+    active:bool,
+    gen:u64,
 
-//wher we get new GenerationIds from 
-pub struct GenManger{
-    items: Vec<EntityActive>,
-    drops:Vec<usize>,//list of all drop
 }
-
-impl GenManger{
+pub struct GenManager {
+    items:Vec<EntityActive>,
+    drops:Vec<usize>,
+}
+impl GenManager {
     pub fn new()->Self{
-        GenManger { 
-            items: Vec::new(), 
-            drops: Vec::new() 
+        GenManager{
+            items:Vec::new(),
+            drops:Vec::new(),
         }
     }
-
-    pub fn next (&mut self) -> GenData{
+    
+    pub fn next(&mut self)->GenData{
         if let Some(loc) = self.drops.pop(){
-            //most recent drop 
-            let ea = &mut  self.items[loc];
+            let ea = &mut self.items[loc];
             ea.active = true;
             ea.gen += 1;
             return GenData{
-                pos: loc,
-                gen: ea.gen
+                pos:loc,
+                gen:ea.gen
             }
         }
-        //if nothing left in drops , add to the end 
-        self.items.push(EntityActive {
-             active: true, 
-             gen: 0
-            }
-        );
+        self.items.push(EntityActive{active:true,gen:0});
         return GenData{
-            gen: 0,
-            pos: self.items.len() - 1,
-        };
-
+            gen:0,
+            pos:self.items.len()-1,
+        }
     }
-
+    
     pub fn drop(&mut self, g:GenData){
         if let Some(ea) = self.items.get_mut(g.pos){
-            if ea.active && ea.gen == g.gen {
-                //dont drop newer item than given 
+            if ea.active && ea.gen == g.gen{
                 ea.active = false;
                 self.drops.push(g.pos);
-
             }
         }
-    }
-}
-
-#[cfg(test)]
-mod tests{
-    use super::*;
-    #[test]
-    fn test_items_drop(){
-        let mut gm = GenManger::new();
-
-        let  g = gm.next();
-        assert_eq!(g,GenData{gen:0,pos:0});
-        let g2 = gm.next();
-        gm.next();
-        gm.next();
-        gm.drop(g2);
-        let g3 = gm.next();
-        assert_eq!(g3, GenData{gen:1,pos:1})
     }
 }
