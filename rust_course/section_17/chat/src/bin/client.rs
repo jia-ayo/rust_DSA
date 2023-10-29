584ea33dc3d3346292023ac2d72bd730fdd20755
jia-ayo <milkjia11@gmail.com>
2023-10-29 20:37:30 +0100

    server msg

--- rust_course/section_17/chat/src/bin/client.rs
+++ rust_course/section_17/chat/src/bin/client.rs
@@ -7,69 +7,67 @@ use chat::utils::{self, ChatResult};
 use chat::{Client, Server};
 use futures_lite::future::FutureExt;
 
-fn get_value(mut input: &str) -> Option<(&str, &str)>{
+fn get_value(mut input: &str) -> Option<(&str, &str)> {
     input = input.trim_start();
 
-    if input.is_empty(){
+    if input.is_empty() {
         return None;
     }
 
-    match input.find(char::is_whitespace){
+    match input.find(char::is_whitespace) {
         Some(whitespace) => Some((&input[0..whitespace], &input[whitespace..])),
         None => Some((input, "")),
     }
 }
 
-fn parse_input(line: &str)-> Option<Client>{
-    let(input, remainder) = get_value(line)?;
+fn parse_input(line: &str) -> Option<Client> {
+    let (input, remainder) = get_value(line)?;
     if input == "join" {
         let (chat, remainder) = get_value(remainder)?;
-        if !remainder.trim_start().is_empty(){
-            return  None;
+        if !remainder.trim_start().is_empty() {
+            return None;
         }
 
-        return Some(Client::Join { chat_name: Arc::new(chat.to_string()), 
+        return Some(Client::Join {
+            chat_name: Arc::new(chat.to_string()),
         });
-    }
-    else if input == "post" {
-         let (chat, remainder) = get_value(remainder)?;
-         let message = remainder.trim_start().to_string();
+    } else if input == "post" {
+        let (chat, remainder) = get_value(remainder)?;
+        let message = remainder.trim_start().to_string();
 
-         return Some(Client::Post { chat_name: Arc::new(chat.to_string()), message: Arc::new(message),
+        return Some(Client::Post {
+            chat_name: Arc::new(chat.to_string()),
+            message: Arc::new(message),
         });
-    }
-    else {
+    } else {
         println!("Unregognized input {:?}", line);
-        return  None;
+        return None;
     }
 }
 
-
-async fn send(mut send: net::TcpStream) -> ChatResult<()>{
+async fn send(mut send: net::TcpStream) -> ChatResult<()> {
     println!("Options: \njoin CHAT \npost CHAT Messsage");
 
-    let mut options =io::BufReader::new(io::stdin()).lines();
+    let mut options = io::BufReader::new(io::stdin()).lines();
 
     while let Some(option_result) = options.next().await {
         let opt = option_result?;
-        let mut req = match parse_input(&opt){
+        let mut req = match parse_input(&opt) {
             Some(req) => req,
-            None=> continue,
+            None => continue,
         };
         utils::send_json(&mut send, &mut req).await?;
         send.flush().await?;
-
     }
     Ok(())
 }
 
-
-async fn message(server: net::TcpStream) -> ChatResult<()>{
+async fn message(server: net::TcpStream) -> ChatResult<()> {
     let buf = io::BufReader::new(server);
     let mut stream = utils::receive(buf);
     while let Some(msg) = stream.next().await {
         match msg? {
-            Server::Message {chat_name, message} =>{
+            Server::Message { chat_name, message } => {
                 println!("Chat Name: {}\n, Message: {}\n", chat_name, message);
             }
             Server::Error(message) => {
@@ -89,7 +87,7 @@ fn main() -> ChatResult<()> {
         let replies = message(socket);
 
         replies.race(send).await?;
-            
+
         Ok(())
     })
 }
--- rust_course/section_17/chat/src/bin/server/main.rs
+++ rust_course/section_17/chat/src/bin/server/main.rs
@@ -1,1 +1,13 @@
+use async_std::{task, net};
+use chat::utils::ChatResult;
+use std::sync::Arc;
+use async_std::prelude::*;
+mod connection;
+mod chats;
+mod chats_map;
 
+use connection::handle;
+
+fn main() -> ChatResult<()>{
+    let addr = std::env::args().nth(1)
+}
