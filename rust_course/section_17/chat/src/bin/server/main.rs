324c7203309ffde06d5f8f08dd82d9b109783f90
jia-ayo <milkjia11@gmail.com>
2023-11-01 05:22:14 +0100

    chat works

--- rust_course/section_17/chat/src/bin/server/main.rs
+++ rust_course/section_17/chat/src/bin/server/main.rs
@@ -1,6 +1,7 @@
 use async_std::{task, net};
 use chat::utils::ChatResult;
-use std::sync::Arc;
+use serde_json::error;
+use std::{sync::Arc, result};
 use async_std::prelude::*;
 mod connection;
 mod chats;
@@ -8,6 +9,26 @@ mod chats_map;
 
 use connection::handle;
 
+fn log_error(result: ChatResult<()>){
+    if let Err(error) = result{
+        println!("Error {}", error);
+    }
+}
+
 fn main() -> ChatResult<()>{
-    let addr = std::env::args().nth(1)
+    let addr = std::env::args().nth(1).expect("server Address");
+    let chat_table = Arc::new(chats_map::ChatTracker::new());
+
+    async_std::task::block_on(async{
+        let listener = net::TcpListener::bind(addr).await?;
+        let mut new_connection = listener.incoming();
+        while let Some(socket_result) = new_connection.next().await {
+            let socket = socket_result?;
+            let chats = chat_table.clone();
+            task::spawn(async{
+                log_error(handle(socket, chats).await);
+            });
+        }
+        Ok(())
+    })
 }
