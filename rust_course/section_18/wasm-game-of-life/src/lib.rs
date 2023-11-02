3d814000b32cf349ab9fd267edad03159458f92c
jia-ayo <milkjia11@gmail.com>
2023-11-02 05:33:03 +0100

    wasm init

---
+++ rust_course/section_18/wasm-game-of-life/src/lib.rs
@@ -0,0 +1,14 @@
+mod utils;
+
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+extern "C" {
+    fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn greet(name: &str) {
+    // let greet_person = ("Hello {}", name);
+    alert(name);
+}
