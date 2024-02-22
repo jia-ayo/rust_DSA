9ffed18dc355bc4092946daec1705fec38c32dde
jia-ayo <milkjia11@gmail.com>
2024-02-22 21:25:20 +0100

    styling of output

--- rust_systems/jia_pipe_viewer/src/stats.rs
+++ rust_systems/jia_pipe_viewer/src/stats.rs
@@ -1,11 +1,18 @@
 use crossbeam::channel::Receiver;
-use std::io::Result;
+use crossterm::{
+    cursor, execute,
+    style::{self, Color, PrintStyledContent},
+    terminal::{Clear, ClearType},
+};
+use std::io::{self, Result, Stderr, Write};
 use std::time::{Duration, Instant};
 
 pub fn stats_loop(silent: bool, stats_rx: Receiver<usize>) -> Result<()> {
     let mut total_bytes = 0;
     let start = Instant::now();
     let mut timer = Timer::new();
+    let mut stderr = io::stderr();
+
     loop {
         //recive bytes
         let num_bytes = stats_rx.recv().unwrap();
@@ -14,11 +21,11 @@ pub fn stats_loop(silent: bool, stats_rx: Receiver<usize>) -> Result<()> {
         total_bytes += num_bytes;
         if !silent && timer.ready {
             timer.ready = false;
-            eprint!(
-                "\rbytes: {} Time: {}s Rate: [{:.0}b/s]",
+            output_progress(
+                &mut stderr,
                 total_bytes,
                 start.elapsed().as_secs().as_time(),
-                rate_per_second
+                rate_per_second,
             );
         }
 
@@ -28,12 +35,25 @@ pub fn stats_loop(silent: bool, stats_rx: Receiver<usize>) -> Result<()> {
     }
     if !silent {
         eprintln!(
-            "\rTotal_byte: {} Time: {}s",
-            total_bytes,
-            start.elapsed().as_secs().as_time()
         );
     }
     Ok(())
+}
+
+fn output_progress(stderr: &mut Stderr, bytes: usize, elapsed: String, rate: f64) {
+    let bytes = style::style(format!("Bytes: {} ", bytes)).with(Color::Red);
+    let elapsed = style::style(elapsed).with(Color::Green);
+    let rate = style::style(format!("Rate: [{:.0}b/s] ", rate)).with(Color::Blue);
+    let _ = execute!(
+        stderr,
+        cursor::MoveToColumn(0),
+        Clear(ClearType::CurrentLine),
+        PrintStyledContent(bytes),
+        PrintStyledContent(elapsed),
+        PrintStyledContent(rate)
+    );
+
+    let _ = stderr.flush();
 }
 
 trait TimeOutput {
@@ -44,7 +64,7 @@ impl TimeOutput for u64 {
     fn as_time(&self) -> String {
         let (hours, left) = (*self / 3600, *self % 3600);
         let (minutes, seconds) = (left / 60, left % 60);
-        format!("{}:{:02}:{:02}", hours, minutes, seconds)
+        format!("Time: {}:{:02}:{:02} ", hours, minutes, seconds)
     }
 }
 
