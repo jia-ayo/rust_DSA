34b22f23923bf97eb5bd82754a9095a0eda562fe
jia-ayo <milkjia11@gmail.com>
2024-06-17 10:58:58 +0100

    game of life

--- rust_course/section_18/wasm-game-of-life/src/lib.rs
+++ rust_course/section_18/wasm-game-of-life/src/lib.rs
@@ -1,5 +1,7 @@
 mod utils;
 
+use std::cell;
+
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
@@ -17,16 +19,15 @@ pub struct Universe {
     cells: Vec<Cell>,
 }
 
-
 impl Universe {
     fn get_index(&self, row: u32, column: u32) -> usize {
         (row * self.width + column) as usize
     }
 
-    fn live_neighor_count(&self , row: u32, column: u32) -> u8 {
+    fn live_neighor_count(&self, row: u32, column: u32) -> u8 {
         let mut count = 0;
-        for delta_row in [self.height - 1, 0, 1].iter().cloned(){
-            for delta_col in [self.width - 1, 0, 1].iter().cloned(){
+        for delta_row in [self.height - 1, 0, 1].iter().cloned() {
+            for delta_col in [self.width - 1, 0, 1].iter().cloned() {
                 if delta_row == 0 && delta_col == 0 {
                     continue;
                 }
@@ -35,10 +36,78 @@ impl Universe {
                 let neighbor_col = (column + delta_col) % self.width;
                 let idx = self.get_index(neighbor_row, neighbor_col);
                 count += self.cells[idx] as u8;
-
-
             }
         }
         count
+    }
+}
+
+#[wasm_bindgen]
+impl Universe {
+    pub fn tick(&mut self) {
+        let mut next = self.cells.clone();
+
+        for row in 0..self.height {
+            for col in 0..self.width {
+                let idx = self.get_index(row, col);
+                let cell = self.cells[idx];
+                let live_neighbors = self.live_neighor_count(row, col);
+
+                let next_cell = match (cell, live_neighbors) {
+                    //Rule 1: Any live cell with fewer than 2 live neighbors die because of underpopulation
+                    (Cell::Alive, x) if x < 2 => Cell::Dead,
+                    //rule 2: any live cell with two or three live neighbors lives on
+                    (Cell::Alive, 2) | (Cell::Alive, 3) => Cell::Alive,
+
+                    //rule 3: any live cell with more than three live neighbors dies be of overpopulation
+                    (Cell::Alive, x) if x > 3 => Cell::Dead,
+                    //rule 4: any dead cell with exectly three neighbors becomes a live cell due to reproduction
+                    (Cell::Dead, 3) => Cell::Alive,
+                    //otherwise cell remains in the same state as before
+                    (otherwise, _) => otherwise,
+                };
+                next[idx] = next_cell;
+            }
+        }
+        self.cells = next;
+    }
+
+    pub fn new() -> Universe {
+        let width = 64;
+        let height = 64;
+
+        let cells = (0..height * width)
+            .map(|i| {
+                if i % 2 == 0 || i % 7 == 0 {
+                    Cell::Alive
+                } else {
+                    Cell::Dead
+                }
+            })
+            .collect();
+        Universe {
+            width,
+            height,
+            cells,
+        }
+    }
+
+    pub fn render(&self) -> String {
+        self.to_string()
+    }
+}
+
+use std::fmt;
+
+impl fmt::Display for Universe {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        for line in self.cells.as_slice().chunks(self.width as usize) {
+            for &cell in line {
+                let symbol = if cell == Cell::Dead { '◻' } else { '◼' };
+                write!(f, "{}", symbol)?;
+            }
+            write!(f, "\n");
+        }
+        Ok(())
     }
 }
